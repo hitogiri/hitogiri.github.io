35e42e459c9093defccf8d4c63c414cdebd8b1a0
Hitogiri <obieze.isid@gmail.com>
2022-02-21 12:57:45 -0600

    included CNAME file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import ProjectDetailsModal from "./ProjectDetailsModal";
-import { tsParticles } from "tsparticles";
 
 class Projects extends Component {
 
